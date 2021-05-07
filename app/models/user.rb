440cd531f1960ea43c5131582a114a0c9cb7c356
Laguna1 <lagunazoomarket@gmail.com>
2021-05-08 01:30:42 +0300

    Add User model and tests

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
+  validates :password, presence: true, length: { minimum: 5 }
+end
