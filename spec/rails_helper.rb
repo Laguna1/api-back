dd9e52a4f17c6e17d915e4470d9279b7dd923d66
Laguna1 <lagunazoomarket@gmail.com>
2021-05-08 20:58:06 +0300

    Add users controller, routes and request tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,7 +69,7 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
-
+  config.include RequestSpecHelper, type: :request
   # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
