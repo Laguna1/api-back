0ed96bf4b52cefedaebd133439c80fb0d4520577
Laguna1 <lagunazoomarket@gmail.com>
2021-05-09 21:24:27 +0300

    Add sessions controller & update routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      login!
       render json: {
         status: :created,
         user: @user
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :users,  only: %i[create show index] 
+  post '/login', to: 'sessions#create'
+      delete '/logout', to: 'sessions#destroy'
+      get '/logged_in', to: 'sessions#is_logged_in?'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
