9695bc792f4101623985886b5acb15bd7a5b8786
Laguna1 <lagunazoomarket@gmail.com>
2021-05-09 22:07:31 +0300

    Add trackings controller & tests & update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
    
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  get '/logged_in', to: 'sessions#is_logged_in?'
+  get '/logged_in', to: 'sessions#method_logged_in?'
   resources :users,  only: %i[create show index] do
-    resources :activities
+    resources :activities do
+      resources :trackings
+    end
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
