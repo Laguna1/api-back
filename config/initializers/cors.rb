0d2bb597629f20be0cc6cdc0be45e6723d98dc05
Laguna1 <lagunazoomarket@gmail.com>
2021-05-14 06:27:56 +0300

    Check cors for production

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://final-api-backend.herokuapp.com/', 'http://localhost:3001'
 
     resource '*',
       headers: :any,
