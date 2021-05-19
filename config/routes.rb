f714d2e2351c917477a0b7668c0538a542ac2ea3
Laguna1 <lagunazoomarket@gmail.com>
2021-05-19 18:18:36 +0300

    Check correct api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
    
+  post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  get '/logged_in', to: 'sessions#method_logged_in?'
+  # get '/logged_in', to: 'sessions#method_logged_in?'
   resources :users,  only: %i[create show index] do
     resources :activities do
       resources :trackings
