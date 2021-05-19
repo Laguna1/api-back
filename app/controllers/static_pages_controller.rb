5da42e28d9f2d62bcf2333b00b4b5d43eb26fe3b
Laguna1 <lagunazoomarket@gmail.com>
2021-05-19 17:55:05 +0300

    Remove static pages controller & route

--- app/controllers/static_pages_controller.rb
+++
@@ -1,5 +0,0 @@
-class StaticPagesController < ApplicationController
-  def home
-    render json: 'This is where the server side of my React "APP FRONT" application resides'
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
    
-  root 'static_pages#home'
-  post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/logged_in', to: 'sessions#method_logged_in?'
   resources :users,  only: %i[create show index] do
