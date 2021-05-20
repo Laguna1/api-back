7df8c7cd5ae40bed379037ed4e764d8aedfde177
Laguna1 <lagunazoomarket@gmail.com>
2021-05-21 02:36:35 +0300

    Remove comments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
    
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  # get '/logged_in', to: 'sessions#method_logged_in?'
   resources :users,  only: %i[create show index] do
     resources :activities do
       resources :trackings
