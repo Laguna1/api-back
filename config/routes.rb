0d1ba68ca9365b7663f96941a3905592cfb33c55
Laguna1 <lagunazoomarket@gmail.com>
2021-05-22 02:54:59 +0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
    
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users,  only: %i[create show] do
+  resources :users,  only: %i[create show index] do
     resources :activities do
       resources :trackings
     end
