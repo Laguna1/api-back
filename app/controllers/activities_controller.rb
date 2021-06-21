a8c0d9d3ce1077a90df060035cd749c44796ab7a
Laguna1 <lagunazoomarket@gmail.com>
2021-06-21 22:01:16 +0300

    Check activities & trackings controllers and tests for it

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,6 +4,7 @@ class ActivitiesController < ApplicationController
 
   def index
     @activities = current_user.activities.all
+
     render json: serializer.new(@activities)
   end
 
@@ -13,23 +14,22 @@ class ActivitiesController < ApplicationController
 
   def create
     activities = current_user.activities
+
     @activity = activities.build(activity_params)
-    if activities.exists?(item: activity_params[:item])
+
+    if activities.exists?(day: activity_params[:day])
       render json: { item: ['has already been taken'] }, status: :unprocessable_entity
     elsif @activity.save
       render json: serializer.new(@activity), status: :created, location: @activity
     else
-
       render json: @activity.errors, status: :unprocessable_entity
     end
   end
 
   def update
     if @activity.update(activity_params)
-
       render json: serializer.new(@activity)
     else
-
       render json: @activity.errors, status: :unprocessable_entity
     end
   end
