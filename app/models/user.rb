da1f49b1db6222dea761e6f9cb051d63bb7a59cc
Laguna1 <lagunazoomarket@gmail.com>
2021-06-21 22:15:13 +0300

    Remove User model & controllers users, sessions & tests

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-  has_many :activities
-  has_many :trackings, through: :activities
-
-  validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
-  validates :password, presence: true, length: { minimum: 6 }
-end
