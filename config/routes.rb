6c2c065924d19c111e3d85864c838d6437dfe6b6
Laguna1 <lagunazoomarket@gmail.com>
2021-06-22 21:27:48 +0300

    Add registrations controller & tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
    
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  resources :users,  only: %i[create show index] do
-    resources :activities do
-      resources :trackings
-    end
-  end
+  post 'sign_up', to: 'registrations#create'
 end
